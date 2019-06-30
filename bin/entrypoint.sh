e6cb854d527e665189e6eb1c8fe855783425101d
Fluke667 <Fluke667@gmail.com>
2019-06-30 02:29:25 +0200

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,86 +1,209 @@
 #!/usr/bin/env bash
 #
-# VPNet.io - Virtual Private Network Essential Toolbox
+# VPNet - Virtual Private Network Essential Toolbox
 #
 # https://github.com/acrossfw/vpnet
 #
 
-# shellcheck disable=SC1090
-source "$ACROSSFW_HOME/vpnet-functions.sh"
-vpnet::init_bash "${BASH_SOURCE[0]}" # set all the magic
+vpnet::init_bash() {
+  #
+  # Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+  # http://kvz.io/blog/2013/11/21/bash-best-practices/
+  #
+  set -o errexit
+  set -o pipefail
+  set -o nounset
+  # set -o xtrace
 
-declare __root
+  local source=$1
+  if [[ -z "$source" ]]; then
+    echo "ERROR: vpnet::init_bash must have BASH_SOURCE[0] as arg1"
+    return 1
+  fi
 
-main() {
-  local arg1=${1:-}
+  # Set magic variables for current file & dir
+  declare -gx __dir
+  declare -gx __file
+  declare -gx __base
+  declare -gx __root
 
-  vpnet::init_env_var
+  __dir="$(cd "$(dirname "$source}")" && pwd)"
+  __file="${__dir}/$(basename "${source}")"
+  __base="$(basename "${__file}" .sh)"
+  __root="${ACROSSFW_HOME:-/acrossfw}" # "$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+}
 
-  echo
-  echo "Starting VPNet Docker v$(head -1 "${__root}/VERSION") #${VERSION_HASH}"
-  echo
-  echo "  https://github.com/acrossfw/vpnet"
-  echo
-  echo "  "
-  echo "  Build by $BUILD_HOST($BUILD_IP) on $BUILD_DATE"
-  echo "  Run as $(hostname -f) with IP $WANIP"
-  echo
+vpnet::init_env_var() {
+  if vpnet::is_docker ; then
+    # shellcheck disable=SC1090
+    source "$ACROSSFW_HOME/ENV.build"
 
-  vpnet::check_env
+    [[ -f "$ACROSSFW_HOME/ENV.config" ]] && {
+      # shellcheck disable=SC1090
+      source "$ACROSSFW_HOME/ENV.config"
+    }
+  fi
 
-  vpnet::init_system
-  vpnet::init_network
-  # vpnet::init_service
+  declare -gx WANIP
+  WANIP=$(curl -Ss ifconfig.io)
 
-  case "$arg1" in
-    start)
-      # Use baseimage-docker's init system.
-      # CMD ["/sbin/my_init"]
+  vpnet::init_host_id
+}
 
-      exec my_init
+vpnet::init_host_id() {
+  local id
+  id=$(ip addr show docker0 | grep ether | awk '{print $2}' | awk -F: '{print $5$6}')
+  HOSTNAME=${HOSTNAME/vpnet./vpnet-$id.}
+}
 
-      err_code=$?
-      echo "ERROR: my_init exit with error code $err_code"
-      sleep 1
-      exit $err_code
-      ;;
+vpnet::check_env() {
+  [[ "$(id -u)" = 0 ]] || {
+    echo "ERROR: must run as root"
+    return 1
+  }
 
-    test)
-      bats_cmd="bats ${__root}/test/*.bats"
+  [[ "${ACROSSFW_HOME}" ]] || {
+    echo "ERROR: ACROSSFW_HOME environment variable not defined"
+    return 1
+  }
 
-      echo
-      echo "Start testing ... "
-      echo
+  return 0
+}
 
-      if ! $bats_cmd && (cd service/vpnet && npm test && cd -); then
-        echo
-        echo "ERROR: Test FAIL"
-        echo
+vpnet::init_config() {
+  config_file=$1
+  [[ "$config_file" =~ ^/ ]] || {
+    vpnet::log "ERROR: vpnet::init_config need absolute filename start with '/'"
+    return 1
+  }
 
-        exit -1
-      fi
+  #
+  # $__dir is the magic variable set by init_bash
+  # standard for the script execute dir
+  #
+  template_file="${__dir}/root${config_file}"
+  [ -f "$template_file" ] || {
+    vpnet::log "ERROR: vpnet::init_config cant find '$template_file'! must run in 'service/SRV/run'"
+    return 1
+  }
 
-      echo
-      echo "Test PASS"
-      echo
+  vpnet::is_docker || {
+    vpnet::log "ERROR: vpnet::init_config can only run inside docker(or it will overwrite root filesystem)"
+    exit 1
+  }
 
-      exit 0
-      ;;
+  echo "vpnet::init_config initing $config_file from $template_file ..."
+  # Templating with Linux in a Shell Script
+  # http://serverfault.com/a/699377/276381
+  template="$(cat "${template_file}")"
+  eval "echo \"${template}\"" > "$config_file"
+}
 
-    bash|sh|shell)
-      echo "Creating shell..."
-      exec /bin/bash -s
-      exit $?
-      ;;
+vpnet::is_docker() {
+  # XXX simulate docker for test
+  # return 0
 
-    *)
-      echo "ERROR: Unsupport arg $arg1"
-      exit -1
+  # http://stackoverflow.com/a/20012536/1123955
+  if [[ $(sort -n /proc/1/cgroup | head -1) =~ /$ ]]; then
+    # end with '/', should be the host
+    return 1
+  else
+    # end with container string, should insdie docker
+    return 0
+  fi
+}
+
+#
+# System & Networking Initialization
+#
+vpnet::init_system() {
+  echo "Setting hostname to $HOSTNAME ..."
+  # XXX: this will not work in --net=host mode
+  # https://github.com/docker/docker/issues/5708
+  hostname "$HOSTNAME" || echo "WARN: set hostname fail"
+
+  echo "Disabling coredump ..."
+  sysctl fs.suid_dumpable=0
+  ulimit -S -c 0
+  echo "* hard core 0" >> /etc/security/limits.conf
+}
+
+vpnet::init_network() {
+  echo "Setting ip forwarding to 1 ..."
+  sysctl -w net.ipv4.ip_forward=1           || echo "WARN: sysctl ip_forward fail"
+  sysctl -w net.ipv4.conf.all.forwarding=1  || echo "WARN: sysctl ipv4 forwarding fail"
+  sysctl -w net.ipv6.conf.all.forwarding=1  || echo "WARN: sysctl ipv6 forwarding fail"
+  sysctl -w net.ipv6.conf.all.proxy_ndp=1   || echo "WARN: sysctl proxy_ndp fail"
+  echo 1 > /proc/sys/net/ipv4/route/flush   || echo "WARN: sysctl ipv4 flush fail"
+
+  echo "Enabling Google BBR for TCP ..."
+  sysctl -w net.core.default_qdisc=fq           || echo "WARN: sysctl default_qdisc fail"
+  sysctl -w net.ipv4.tcp_congestion_control=bbr || echo "WARN: sysctl tcp_congestion_control fail"
+
+  # XXX does there always be `docker0` in docker ???
+  echo "Setting network filter ..."
+  iptables -t nat -A POSTROUTING -s 10.0.0.0/8      -o docker0 -j MASQUERADE || echo "WARN: iptables fail"
+  iptables -t nat -A POSTROUTING -s 172.16.0.0/12   -o docker0 -j MASQUERADE || echo "WARN: iptables fail"
+  iptables -t nat -A POSTROUTING -s 192.168.0.0/16  -o docker0 -j MASQUERADE || echo "WARN: iptables fail"
+
+  # ip6tables -t nat -A POSTROUTING -s 2a00:1450:400c:c05::/64 -o docker0 -j MASQUERADE
+
+  # XXX no need ? iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+}
+
+vpnet::get_user_home() {
+  local user_name=${1:-''}
+  local __resultvar=${2:-''}
+
+  case $__resultvar in
+    '__resultvar'|'__user_home'|'user_name'|'error_code')
+      vpnet::log "ERROR: vpnet::get_user_home __user_home is reserved by this function"
+      return 1
       ;;
   esac
 
-  echo "ERROR: Should not run to here!"
-  exit -1
+  local __user_home
+  local error_code
+
+  __user_home=$(eval echo ~"$user_name")
+  error_code=0
+
+  # non-exist user will not resolve and keep the origin string, which has a leading '~'
+  [[ "$__user_home" =~ ^~ ]] && {
+    vpnet::log "ERROR: vpnet::get_user_home can not find home for user: %s" "$user_name"
+    error_code=1 # no such user
+  }
+
+  case "${#@}" in
+    1)
+      echo "$__user_home"
+      ;;
+    2)
+      vpnet::set_var_value "$__resultvar" "$__user_home"
+      ;;
+    *)
+      vpnet::log "ERROR: vpnet::get_user_home should take 1 or 2 args"
+      error_code=1
+      ;;
+  esac
+
+  return "$error_code"
 }
 
-main "$@"
+# http://www.linuxjournal.com/content/return-values-bash-functions
+vpnet::set_var_value() {
+  local __resultvar=$1
+  local __value=$2
+
+  if [[ "$__resultvar" = "__resultvar" || "$__resultvar" = "__value" ]]; then
+    vpnet::log "ERROR: vpnet::set_var_value reserved name: __resultvar & __value"
+    return 1
+  fi
+  # declare global variable http://stackoverflow.com/q/9871458/1123955
+  eval "$__resultvar='$__value'"
+}
+
+vpnet::log() {
+  eval "printf $*" >&2
+  echo >&2
+}
